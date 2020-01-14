188a7cd6d893315ca1a3164791bc035e9b1d5cb9
Joshua Kingswell <joshua.kingswell@rradar.com>
2020-01-14 18:46:48 +0000

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Mussolini Musli Mountain/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Mussolini Musli Mountain/i);
   expect(linkElement).toBeInTheDocument();
 });
